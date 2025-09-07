8576ac99caa4ec83a63b020af45e7eb0565e68e7
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-09-07 17:07:39 +0300

    дб и миграции

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,11 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
+    description: str = (
+         'Приложение предоставит возможность бронировать помещения'
+         ' на определённый период времени '
+    )
+    database_url: str
 
     class Config:
         env_file = '.env'
