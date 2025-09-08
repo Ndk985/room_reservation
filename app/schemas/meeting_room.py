596b5cc8f779defad5fba1439d2d6776df75a82d
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-09-08 15:39:57 +0300

    CRUD, создание и чтение, обработка ошибок

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -1,21 +1,19 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, validator
 
 
 class MeetingRoomCreate(BaseModel):
     name: str = Field(..., max_length=100)
     description: Optional[str]
 
-    @field_validator('name')
+    @validator('name')
     @classmethod
     def validate_name(cls, value: str) -> str:
-        # Проверка на пустую строку
         if not value.strip():
             raise ValueError(
                 'Название не может быть пустым или состоять только из пробелов'
             )
-        # Проверка длины (уже есть в Field, но для надежности можно добавить)
         if len(value) > 100:
             raise ValueError('Название не может превышать 100 символов')
 
