15810c58f08e10f0e6f18c0f6d68c6c462566a6f
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-09-08 20:09:35 +0300

    ответы на запросы

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -3,9 +3,13 @@ from typing import Optional
 from pydantic import BaseModel, Field, validator
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., max_length=100)
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
     description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_length=1, max_length=100)
 
     @validator('name')
     @classmethod
@@ -18,3 +22,10 @@ class MeetingRoomCreate(BaseModel):
             raise ValueError('Название не может превышать 100 символов')
 
         return value
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
